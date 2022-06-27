7951e250486b4d67be8a7ff12415183f152feac4
Aditya Kumar Singh <74051207+MrAdiii@users.noreply.github.com>
2022-06-27 15:21:20 +0530

    initial commit

---
+++ functions.py
@@ -0,0 +1,58 @@
+from telethon import TelegramClient, events, Button
+from telethon.tl.types import PeerChannel
+
+approval_keyboard = [
+    [
+        Button.inline("Approve", b"approved"),
+        Button.inline("Reject", b"rejected")
+    ]
+]
+
+approved_keyboard = [
+    [
+        Button.inline("Delete", b"delete")
+    ]
+]
+
+rejected_keyboard = [
+    [
+        Button.inline("Undo", b"undo")
+    ]
+]
+
+
+async def client_forward(client: TelegramClient, bot_id: int, client_black_list: list):
+    @client.on(events.NewMessage(chats=client_black_list, blacklist_chats=True, incoming=True))
+    async def forward_message(event):
+        if isinstance(event.message.peer_id, PeerChannel):  # forward only channel messages to bot
+            print(event.stringify(), "\n Client Forward")
+            await client.send_message(bot_id, event.message)
+
+
+async def bot_forward(bot: TelegramClient, backend: int, bot_allowed_list: list):
+    @bot.on(events.NewMessage(chats=bot_allowed_list))
+    async def forward_message(event):
+        print(event.stringify(), "\n Bot Forward")
+        await bot.send_message(entity=backend, message=event.message, buttons=approval_keyboard)
+
+
+async def bot_callback(bot: TelegramClient, deals_channel, backend):
+    @bot.on(events.CallbackQuery(chats=backend))
+    async def callback_handler(event):
+        print(event.stringify(), "\n Bot CallBack")
+        message = await event.get_message()
+        print(message)
+        if event.data == b'approved':
+            print("Approved")
+            await bot.send_message(entity=deals_channel, message=message.message)
+            await bot.edit_message(entity=backend, message=message.id, buttons=approved_keyboard)
+        elif event.data == b'rejected':
+            print("Rejected")
+            await bot.edit_message(entity=backend, message=message.id, buttons=rejected_keyboard)
+        elif event.data == b'delete':
+            print("Deleted")
+            await bot.delete_messages(entity=backend, message_ids=message.id)
+        else:
+            print("Re-Publishing")
+            await bot.send_message(entity=backend, message=message, buttons=approval_keyboard)
+            await bot.edit_message(entity=backend, message=message.id)
---
+++ main.py
@@ -0,0 +1,25 @@
+from telethon import TelegramClient
+from telethon.tl.types import PeerChannel, PeerUser
+
+from functions import client_forward, bot_forward, bot_callback
+import os
+
+api_id = int(os.environ['API_ID'])
+api_hash = os.environ['API_HASH']
+bot_token = os.environ['BOT_TOKEN']
+
+bot_id = int(os.environ['BOT_ID'])
+backend = int(os.environ['BACKEND'])
+user_id = PeerUser(int(os.environ['USER_ID']))
+deals_channel = PeerChannel(int(os.environ['DEALS_CHANNEL']))
+client_black_list = [backend, deals_channel]
+bot_allowed_list = [backend, user_id]
+
+client = TelegramClient('anon', api_id, api_hash)
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
+with client:
+    client.loop.create_task(client_forward(client, bot_id, client_black_list))
+    client.loop.create_task(bot_forward(bot, backend, bot_allowed_list))
+    client.loop.create_task(bot_callback(bot, deals_channel, backend))
+    client.loop.run_forever()
